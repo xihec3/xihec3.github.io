f38df7abe2341e0f9bb8cc8302b3f0f9dbe68df8
xihe <xihec3@macbook.local>
2021-08-02 11:11:00 +0800

    Add third graph

--- js/vis.js
+++ js/vis.js
@@ -40,7 +40,7 @@ function startVis() {
   document.getElementById("selectDiv").style.width = "150px";
   addCheckboxes();
   slide_num = 0;
-  showSlide();
+  updateSelect();
 }
 
 function addPrevAndNextButton() {
@@ -48,7 +48,7 @@ function addPrevAndNextButton() {
 }
 
 function addCheckboxes() {
-  document.getElementById("selectDiv").innerHTML = "<p>Sports to include:</p><ul>"
+  document.getElementById("selectDiv").innerHTML = "<p>Select sports to include:</p><ul>"
   sports.forEach((sport, i) => document.getElementById("selectDiv").innerHTML += "<li><input type='checkbox' class='sport_cbx' onclick='updateSelect()' checked value='".concat(sport, "'>", sport, "</li>"));
   document.getElementById("selectDiv").innerHTML += "</ul>"
 }
@@ -75,7 +75,7 @@ function addMedal(data) {
 function showSlide() {
   if (slide_num == 0) {
     clear();
-    document.getElementById("textDiv").innerHTML = "<h4>Who won the most medals?</h4><p>The history high is Phllip.</p>";
+    document.getElementById("textDiv").innerHTML = "<h4>Who won the most medals?</h4><p>In 2016 Olympics, Swimmer Michael Phelps won the 6 total medals on his journey to becoming the most decorated Olympian. He also holds the all-time records for Olympic gold medals with 23 gold medal winning.</p><p>Out of the 11,238 athletes participating in 306 events, 1857 got at least one medal.</p>"
     selected_athletes.sort(function(a,b){return addMedal(b)- addMedal(a);});
     show_athletes = selected_athletes.slice(0, 20);
     show_athlete_names = show_athletes.map(d=>d.name);
@@ -86,7 +86,6 @@ function showSlide() {
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); 
     y = d3.scaleBand().domain(show_athlete_names).range([0, height]).padding(0.1);
-    console.log(y.bandwidth())
     svg.append("g")
       .call(d3.axisLeft(y).tickSizeOuter(0));
 
@@ -108,27 +107,141 @@ function showSlide() {
        .enter().append("g")
          .attr("fill", d => color(d.key))
          .selectAll("rect")
-         .data(function(d) {console.log(d); return d;} )  // // enter a second time = loop subgroup per subgroup
+         .data(function(d) { return d;} )  // // enter a second time = loop subgroup per subgroup
          .enter().append("rect")
          .attr("x", d => x(d[0]) + 2)
          .attr("y", d => y(d.data.name))
          .attr("height", y.bandwidth())
          .attr("width", d => x(d[1]) - x(d[0]));
 
-  } else {
-    console.log("unimplemented");
-  } 
+    if (first_time) {
+      document.getElementById("graph").innerHTML += "<g><path class='annotation_line' d='M 480 400 L 650 400 M 410 410 L 480 400'/><text x='485' y='392' fill='black'>'Unluckiness?'</text><text x='485' y='418' fill='black'>Grasse got 3 medals but</text><text x='485' y='433' fill='black'>no gold medal.</text></g>" 
+      first_time = false;
+    }
+  } else if (slide_num == 1) {
+    clear();
+    document.getElementById("textDiv").innerHTML = "<h4>What are the prime years age Who won the most medals?</h4><p>In 2016 Olympics, Swimmer Michael Phelps won the 6 total medals on his journey to becoming the most decorated Olympian. He also holds the all-time records for Olympic gold medals with 23 gold medal winning.</p><p>Out of the 11,238 athletes participating in 306 events, 1857 got at least one medal.</p>"
+    selected_athletes.sort(function(a,b){return addMedal(b)- addMedal(a);});
+    show_athletes = selected_athletes.slice(0, 20);
+    show_athlete_names = show_athletes.map(d=>d.name);
+    show_athlete_count = show_athletes.map(d=>addMedal(d));
+    max_number = Math.max(...show_athlete_count);
+
+    svg = d3.select("svg")
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); 
+    y = d3.scaleBand().domain(show_athlete_names).range([0, height]).padding(0.1);
+    svg.append("g")
+      .call(d3.axisLeft(y).tickSizeOuter(0));
+
+    x = d3.scaleLinear().domain([0,max_number]).range([0, width]);
+    svg.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(d3.axisBottom(x).ticks(max_number+1).tickFormat(d3.format("d")));
+    
+    var subgroups = ["gold", "silver", "bronze"];
+    var color = d3.scaleOrdinal()
+                  .domain(subgroups)
+                  .range(['#fee101','#cccccc','#a05822']);
+    var stackedData = d3.stack()
+                        .keys(subgroups)
+                        (show_athletes)
+    svg.append("g")
+       .selectAll("g")
+       .data(stackedData) // Enter in the stack data = loop athlete per athlete
+       .enter().append("g")
+         .attr("fill", d => color(d.key))
+         .selectAll("rect")
+         .data(function(d) { return d;} )  // // enter a second time = loop subgroup per subgroup
+         .enter().append("rect")
+         .attr("x", d => x(d[0]) + 2)
+         .attr("y", d => y(d.data.name))
+         .attr("height", y.bandwidth())
+         .attr("width", d => x(d[1]) - x(d[0]));
+
+    if (first_time) {
+      document.getElementById("graph").innerHTML += "<g><path class='annotation_line' d='M 410 340 L 480 400 L 650 400 M 410 410 L 480 400 M 410 458 L 480 400'/><text x='485' y='392' fill='black'>'Unluckiness'</text><text x='485' y='418' fill='black'>Some athletes got 3 medals</text><text x='485' y='433' fill='black'>but no gold medal.</text></g>" 
+      first_time = false;
+    }
+  } else if (slide_num == 2) {
+    clear();
+    document.getElementById("textDiv").innerHTML = "<h4>Did nationality affect medal winning?</h4><p>Athletes from 207 countries participated in the 2016 Summer Olympics. Athletes from 87 countries won medals. The U.S. led the medal table, winning 121 medals.</p><p>The chart on the right shows the average number of medals won per athlete for top 20 countries.</p>"
+    var country_map = new Map();
+    for (var i = 0; i < selected_athletes.length; i++) {
+        ctr = selected_athletes[i].nationality;
+        if (country_map.has(ctr)) {
+          existing = country_map.get(ctr);
+          country_map.set(ctr, {cnt: existing.cnt+1, gold: existing.gold + parseInt(selected_athletes[i].gold), silver: existing.silver + parseInt(selected_athletes[i].silver), bronze: existing.bronze + parseInt(selected_athletes[i].bronze)});
+        } else {
+          country_map.set(ctr, {cnt: 1, gold: parseInt(selected_athletes[i].gold), silver: parseInt(selected_athletes[i].silver), bronze: parseInt(selected_athletes[i].bronze)});
+        }
+    }
+
+    avg = [];
+    country_map.forEach((value, key) => 
+{
+  avg.push({name: key, gold: value.gold/value.cnt, silver: value.silver/value.cnt, bronze: value.bronze/value.cnt});
+});
+
+    avg.sort(function(a,b){return (b.gold+b.silver+b.bronze) - (a.gold+a.silver+a.bronze);});
+    show_countries = avg.slice(0, 20);
+    show_country_names = show_countries.map(d=>d.name);
+    show_country_avg = show_countries.map(d=>d.gold+d.silver+d.bronze);
+    max_number = Math.max(...show_country_avg);
+
+    svg = d3.select("svg")
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); 
+    y = d3.scaleBand().domain(show_country_names).range([0, height]).padding(0.1);
+    svg.append("g")
+      .call(d3.axisLeft(y).tickSizeOuter(0));
+
+    x = d3.scaleLinear().domain([0,max_number]).range([0, width]);
+    svg.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(d3.axisBottom(x));
+    
+    var subgroups = ["gold", "silver", "bronze"];
+    var color = d3.scaleOrdinal()
+                  .domain(subgroups)
+                  .range(['#fee101','#cccccc','#a05822']);
+    var stackedData = d3.stack()
+                        .keys(subgroups)
+                        (show_countries)
+    svg.append("g")
+       .selectAll("g")
+       .data(stackedData) // Enter in the stack data = loop athlete per athlete
+       .enter().append("g")
+         .attr("fill", d => color(d.key))
+         .selectAll("rect")
+         .data(function(d) {return d;} )  // // enter a second time = loop subgroup per subgroup
+         .enter().append("rect")
+         .attr("x", d => x(d[0]) + 2)
+         .attr("y", d => y(d.data.name))
+         .attr("height", y.bandwidth())
+         .attr("width", d => x(d[1]) - x(d[0]));
+
+    if (first_time) {
+      document.getElementById("graph").innerHTML += "<g><path class='annotation_line' d='M 460 240 L 530 290 L 700 290 M 390 340 L 530 290'/><text x='535' y='282' fill='black'>'Bronze Only'</text><text x='535' y='308' fill='black'>Norway got 4 bronze medals;</text><text x='535' y='322' fill='black'>Nigeria got 1 bronze medal.</text></g>" 
+      first_time = false;
+    }
+  }  
 }
 
 function clear() {
   d3.select('svg').selectAll("*").remove();
+  document.getElementById("textDiv").innerHTML = ""; 
 }
 
 function prev() {
   if (slide_num == 1 || slide_num == 2) {
     slide_num -= 1;
     first_time = true;
-    showSlide();
+    const checkboxes = document.getElementsByClassName('sport_cbx');
+    for (var i=0; i<checkboxes.length; i++) {
+      checkboxes[i].checked = true;
+    }
+    updateSelect();
   } else {
     alert("You are viewing the first slide.");
   }
@@ -138,7 +251,11 @@ function next() {
   if (slide_num == 0 || slide_num == 1) {
     slide_num += 1;
     first_time = true;
-    showSlide();
+    const checkboxes = document.getElementsByClassName('sport_cbx');
+    for (var i=0; i<checkboxes.length; i++) {
+      checkboxes[i].checked = true;
+    }
+    updateSelect();
   } else {
     alert("You are viewing the last slide.");
   }
